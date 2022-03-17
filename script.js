3de8921f9c1a207f113edd3daea1fc8f4fa1bff0
Jean Artico Benetti <jean.benetti@prod.local>
2022-03-17 17:20:21 -0300

    4_animações

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-$('.click-scroll').click(function () {
-    $('html, body').animate({
-        scrollTop: $("#teste").offset().top
-        }, 2000); // Tempo em ms que a animação irá durar
-});
+let target = null;
+document.addEventListener("DOMContentLoaded",function(){
+    target = document.querySelectorAll('[data-anime]');
+})
+
+const animationClass ='animate'
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
+    target.forEach(function(element){
+        if((windowTop)>element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass);
+        }
+
+    });
+}
+
+window.addEventListener('scroll', function() {
+    animeScroll();
+})
+
+let targetBlur = null;
+document.addEventListener("DOMContentLoaded",function(){
+    targetBlur = document.querySelectorAll('[data-background]');
+})
+
+const blurClass = 'blur'
+
+function blurScroll() {
+    const windowTop = window.pageYOffset;
+    targetBlur.forEach(function(element){
+        if((windowTop)>element.offsetTop){
+            element.classList.add(blurClass);
+        }else{
+            element.classList.remove(blurClass);
+        }
+
+    });
+}
+
+window.addEventListener('scroll', function() {
+    blurScroll();
+})
+
+
